795410aade242cfa18472c511cf68f8a8e10681c
Rolando <roldangel15@gmail.com>
2026-06-01 08:31:15 -0400

    miniproyecto2

---
+++ habilidades.js
@@ -0,0 +1,26 @@
+  
+  
+  let skills= {
+    main: [
+      { name: "HTML5",      icon: "./img/HTML5.png",        nivel: 95 },
+      { name: "CSS3",       icon: "./img/logo-css3.png",    nivel: 90 },
+      { name: "JavaScript", icon: "./img/js.png",           nivel: 88 },
+      { name: "React.js",   icon: "./img/reacjs.png",       nivel: 85 },
+      { name: "Next.js",    icon: "./img/next.png",         nivel: 80 },
+      { name: "Node.js",    icon: "./img/node-js.png",      nivel: 78 },
+      { name: "Tailwind",   icon: "./img/tailwind.png",     nivel: 88 },
+      { name: "WordPress",  icon: "./img/wordpress.png",    nivel: 92 }
+    ],
+    additional: [
+      { name: "PHP", icon: "./img/Php-logo.png" },
+      { name: "MySQL", icon: "./img/mysql.png" },
+      { name: "Photoshop", icon: "./img/photoshop.png" },
+      { name: "Excel", icon: "./img/excel.png" },
+      { name: "M.S.Access", icon: "./img/access.png" },
+      { name: "PowerPoint", icon: "./img/powerpoint.png" },
+      { name: "M.S.Word", icon: "./img/word.png" },
+      { name: "Powerdirector", icon: "./img/powerdirector.png" }
+    ]
+  }
+
+
