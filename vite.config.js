10d325cc9a5cdebb076e53aac5c9d83688380e06
Rolando <roldangel15@gmail.com>
2026-06-02 10:23:26 -0400

    path

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
 import { defineConfig } from 'vite'
+
+import { resolve } from 'path';
+
 import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    
   ],
-
-
-
-
-
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        proyectos: resolve(__dirname, 'proyectos.html'),
+      },
+    },
+  },
 
 })
 
